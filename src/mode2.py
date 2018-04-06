d1e3a0c8e2f10eb494148975b43593c78166d85b
camillefossier <34370603+camillefossier@users.noreply.github.com>
2018-04-06 09:41:29 +0200

    Ajout de detection de verbes
    bon les verbes reguliers/irreguliers je me suis un peu embrouillé je vais revoir ça

--- src/mode2.py
+++ src/mode2.py
@@ -7,6 +7,32 @@ import copy
 vocab = []
 constVocab = []
 
+toBe = [[['I', 'am'], ['Why are you']],
+        [['I', 'was'], ['Why were you']],
+        [['I', 'have'], ['Why have you']]]
+
+translations = [['my', 'your'],
+                ['I', 'you'],
+                ['me', 'you']]
+
+irregular = []
+regular = []
+
+def parseIrregular(irregular_file):
+    global irregular
+    file = open(irregular_file, 'r')
+    for v in file:
+        irregular.append(v.split(','))
+
+def parseRegular(regular_file):
+    global regular
+    file = open(regular_file, 'r')
+    fileW = open('regular.txt', 'w')
+    for v in file:
+        if v != ''
+            irregular.append(v.split(','))
+            fileW.print(v)
+        
 def parseVocab(vocab_file):
     global vocab
     global constVocab
@@ -27,32 +53,43 @@ def parseVocab(vocab_file):
                 break
             else:
                 theme[th][i].append(line[:-1])
-    vocab = theme
-    constVocab = copy.copy(theme)
+    vocab = copy.deepcopy(theme)
+    constVocab = copy.deepcopy(theme)
 
 def run():
     vocab_file = "../vocab/vocab.txt"
+    irregular_file = '../vocab/irregular.txt'
     parseVocab(vocab_file)
+    parseIrregular(irregular_file)
+    parseRegular("../vocab/regular.txt")
     last = 'NIL'
     over = False
     while(not over):
-        print(vocab)
         inp = input('You: ')
-        last = answer(inp)
+        last = answer(inp, last)
         print('Bot: '+last)
         if inp == 'exit':
             over = True
 
-def answer(sentence):
+def answer(sentence, last):
     # ici il faudra decider si on pose une question par rapport à un certain theme
     # ou si on detecte un "je suis X" etc.
- 
-    return questionFinder(sentence)
-
-def questionFinder(sentence):
-    global vocab
-    global constVocab
     sent = tokenise_en(sentence)
+
+    ans = findBe(sent)
+    if ans == False:
+        ans = findVerb(sent)
+    if ans == False:
+        ans = questionFinder(sent)
+    
+    if ans==False:
+        ans = m1.getRandomWord(ans)
+    if ans == last:
+        ans = answer(sentence, last)
+    return ans
+
+def questionFinder(sent):
+    global vocab
     for word in sent:
         i=-1
         for theme in vocab:
@@ -61,17 +98,83 @@ def questionFinder(sentence):
                 
                 if word == words : 
                     if len(theme[1])==0:
-                        print(constVocab[i][1])
-                        theme[1] = constVocab[i][1]
+                        #print(constVocab[i][1])
+                        theme[1] = copy.deepcopy(constVocab[i][1])
                     question = pickQuestion(theme[1])
                     return question
-    return "Hummm"
+    return False
 
 def pickQuestion(theme):
     question = random.choice(theme)
     theme.remove(question)
     return question
 
+# Function to find a use of the verb 'to be'
+# Returns a question with the appropriate tense : why are you, why were you
+# Or return false if there is not this verb
+def findBe(sent):
+    for tense in toBe:
+        w=0
+        while w < len(sent):
+            wTmp = w
+            i = 0
+            ok = True
+            while ok==True and i<len(tense[0]):
+                if sent[wTmp] != tense[0][i]:
+                    ok = False
+                i+=1
+                wTmp+=1
+            if ok==True:
+                return (tense[1][0]+findCOD(sent, wTmp)+'?')
+            w+=1
+    return False
+
+def findVerb(sent):
+    w=0
+    while w < len(sent):
+        if (sent[w]=='I'):
+            return (findTense(sent,w+1)+findCOD(sent, w+2)+' ?')
+        w+=1
+    return False
+
+def findTense(sent, w):
+    word = sent[w]
+    tense = False
+    for v in irregular:
+        if tense == False:
+            tense = getFormula(v, word)
+        else:
+            break
+    if tense == False:
+        for v in regular:
+            tense = getFormula(v, word)
+    return tense
+
+def getFormula(v,word):
+    if word==v[0]:
+        return 'Why do you '+v[0]
+    elif word==v[1]:
+        return 'Why did you '+v[0]
+    elif word[len(word)-1] == 'd' and word[len(word)-2] == 'e':
+        return 'Why did you '+word[:-1]
+    elif word=='will':
+        return 'Why will you'
+    else:
+        return False
+
+def translate(word):
+    for w in translations:
+        if w[0]==word:
+            return w[1]
+        elif w[1]==word:
+            return w[0]
+    return word
+
+def findCOD(sent, w):
+    ans=''
+    for i in range(w, len(sent)):
+        ans+=' '+translate(sent[i])
+    return ans
 
 def tokenise_en(sent):
 
