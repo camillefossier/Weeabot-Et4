e37cb92c5ff7328cf633606cd7b4aeae1e1deed3
root <root@Mohzick.localdomain>
2018-04-13 10:58:14 +0200

    liste manga créée

--- src/mode3.py
+++ src/mode3.py
@@ -1,5 +1,108 @@
+import requests
+import pickle
+import myanimelist.manga
+import myanimelist.session as mals
+
+class Manga(dict):
+
+	def __init__(self, title = None, rank = None, genres = None, nb_favorites = None, 
+	 					score = None, nb_chapters = None, datePublication = None, authors = None, characters = None):
+
+	 	self.title = title
+	 	self.rank = rank
+	 	self.genres = genres
+	 	self.nb_favorites = nb_favorites
+	 	self.score = score
+	 	self.nb_chapters = nb_chapters
+	 	self.datePublication = datePublication
+	 	self.authors = authors
+	 	self.characters = characters
+
+	def informations(self):
+
+		print(self.title)
+		print("rank:", self.rank)
+		for g in self.genres:
+			print(g)
+
+		print("number of favorites:", self.nb_favorites)
+		print ("score:", float(self.score))
+		print("number of chapters:", self.nb_chapters)
+		print("date publication:", self.datePublication)
+		for a in self.authors:
+			print(a)
+		print("les personnages sont: -------")
+		for c in self.characters:
+			print(c)
+		
+
+
+
+def create_manga_list(nombre):
+	session = mals.Session()
+	list_manga = []
+	for i in range (1, nombre):
+		try:
+			l = session.manga(i)
+			title = (l.title)
+			list_manga.append(l)
+		except myanimelist.manga.InvalidMangaError:
+			continue
+	return list_manga
+
+def create_anime_list(nombre):
+	session = mals.Session()
+	list_anime = []
+	for i in range (1, nombre):
+		try:
+			l = session.anime(i)
+			title = l.title
+			list_anime.append(l)
+		except myanimelist.anime.InvalidAnimeError:
+			continue
+	return list_anime
+
 def run():
+	file = open('mangalistfinal', 'r+b')
+	
+	'''manga_list = []
+	list_manga = create_manga_list(500)
+	for manga in list_manga:
+		genres = []
+		for g in manga.genres:
+			genres.append(g.name)
+
+		authors = []
+		for a in manga.authors:
+				authors.append(a.name)
+
+		characters = []
+		for c in manga.characters:
+			characters.append(c.name)
+
+		m = Manga(manga.title, manga.popularity, genres, manga.favorites, float(manga.score[0]), manga.chapters, manga.published[0], authors, characters)
+		manga_list.append(m)
+
+
+	pickle.dump(manga_list, file)
+	print("ajout manga---")'''
+	listemangafini = []
+	listemangafini = pickle.load(file)
+	nbmanga = 0
+	for manga in listemangafini:
+		nbmanga = nbmanga + 1
+		#manga.informations()
+	print(nbmanga)
+
+
+
+
+	
+		
+
+		
+	
+	
+	
     
-    
-    return 0
 
